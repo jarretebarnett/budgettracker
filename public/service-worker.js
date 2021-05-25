5eb04df610d923d1d8bfd87e6ea448c71d618820
Jarrete Barnett <jarretebarnett@gmail.com>
2021-05-25 09:17:41 -0400

    Pushing fixes to manifest, service-work and server files.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,16 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
-  "/",
+  // "/",
   "/index.html",
- //   "/offline.html",
+  // "/offline.html",
   "/manifest.webmanifest",
-  "/assets/css/style.css",
-  "/routes/api.js",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-512x512.png",
+  "/styles.css",
+  // "/routes/api.js",
+  "/index.js",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 // install
